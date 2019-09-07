2295cc091a9b795bb80dd20b04604d1a08f81a72
Rafael Hueb <34714654+rafaelhueb92@users.noreply.github.com>
2019-09-07 13:30:14 -0300

    see

--- src/core/config/env.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongooseConnection:
-    "mongodb+srv://admin:mongo123@cluster0-vd8nv.mongodb.net/test?retryWrites=true",
-    JWT_SECRET:"minhasuperchavesecretaqueninguemvaidescobrir"
-};
